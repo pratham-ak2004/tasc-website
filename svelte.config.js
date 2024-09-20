a73ebb32d8b8aac7c98e90bbacd7f8244f8a34b7
Pratham A Kadekar <pratham20442@gmail.com>
2024-09-20 21:19:44 +0530

    fix: version conflict

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			vercel: {
-				runtime: 'nodejs16.x'
+				runtime: 'nodejs18.x'
 			}
 		})
 	}
